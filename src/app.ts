da433952d94d107c558b088f26475d84894c6efa
Harpeng <gondarev75@gmail.com>
2024-03-05 14:06:16 +0300

    refactoring1

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express, { NextFunction, Request, Response, json } from "express";
-import mongoose from "mongoose";
-import router from "./routes";
-import limiter from "utils/limiter";
-import helmet from "helmet";
-
+// eslint-disable-next-line import/no-extraneous-dependencies
+import express, {
+  NextFunction, Request, Response, json,
+} from 'express';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import mongoose from 'mongoose';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import helmet from 'helmet';
+import router from './routes';
+import limiter from './utils/limiter';
 
 const { PORT = 3000 } = process.env;
 
@@ -17,7 +21,7 @@ app.use(limiter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   req.user = {
-    _id: "65e0cc132525639f15a1b0eb"
+    _id: '65e0cc132525639f15a1b0eb',
   };
 
   next();
@@ -25,12 +29,13 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 
 app.use(router);
 
-
 const connect = async () => {
   try {
-    mongoose.set("strictQuery", true);
+    mongoose.set('strictQuery', true);
     await mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+    console.log('база данных подключена');
     await app.listen(PORT);
+    console.log('сервер запущен');
   } catch (err) {
     console.log(err);
   }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,8 +7,8 @@ const router = Router();
 router.use("/users", userRouter);
 router.use("/cards", cardRouter);
 
-router.get('*', (_req: Request, _res: Response) => {
-  _res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: 'Запрашиваемый ресурс не найден' });
+router.use('*', (_req: Request, _res: Response) => {
+  _res.status(constants.HTTP_STATUS_NOT_FOUND).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 export default router;
